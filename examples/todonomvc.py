a7141ceb6d1cf50955e1c97df0e801a5a190345a
takd4com <takd4com@gmail.com>
2018-02-17 21:59:13 +0900

    add localStorage support to todo demo

--- examples/todonomvc.py
+++ examples/todonomvc.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # dominter todo NO MVC example
 # This is a poor copy of http://todomvc.com/examples.
-import os
 from logging import (getLogger, StreamHandler, basicConfig,
                       DEBUG, INFO, WARN, ERROR)
 
@@ -69,20 +68,84 @@ class MyWindow(Window):
         self.sel_completed.addEventListener('change', self.on_sel_change)
         self.clear_completed.addEventListener('click', self.on_clear_completed)
 
-    def add_todo(self, txt):
+    STORAGE_KEY = 'todos-dominter'
+    TODOS_KEY = 'todos'
+
+    def store_init(self):
+        dic = self.localStorage.get(self.STORAGE_KEY)
+        if dic is None:
+            self.localStorage[self.STORAGE_KEY] = {self.TODOS_KEY: []}
+        else:
+            lst = dic[self.TODOS_KEY]
+            for ent in lst:
+                txt = ent.get('title')
+                checked = ent.get('completed')
+                sid = self.add_todo(txt, checked=checked, store=False)
+                ent['id'] = sid
+            self.localStorage[self.STORAGE_KEY] = dic
+
+    def store_add(self, sid, txt, checked=False):
+        dic = self.localStorage[self.STORAGE_KEY]
+        dic[self.TODOS_KEY].append({'title': txt, 'completed': False, 'id': sid})
+        self.localStorage[self.STORAGE_KEY] = dic
+
+    def store_del(self, sid):
+        dic = self.localStorage[self.STORAGE_KEY]
+        lst = dic[self.TODOS_KEY]
+        for j, x in enumerate(lst):
+            if x['id'] == sid:
+                del lst[j]
+        self.localStorage[self.STORAGE_KEY] = dic
+
+    def store_mod(self, sid, txt=None, checked=None):
+        dic = self.localStorage[self.STORAGE_KEY]
+        for x in dic[self.TODOS_KEY]:
+            if x['id'] == sid:
+                if txt is not None:
+                    x['title'] = txt
+                if checked is not None:
+                    x['completed'] = checked
+                self.localStorage[self.STORAGE_KEY] = dic
+                break
+
+    def onload(self, ev):
+        self.todo_list.childList.clear()
+        self.store_init()
+
+    def add_todo(self, txt, checked=False, store=False):
+        if 0 == len(txt):
+            return
         doc = self.document
         itm = doc.label(txt)
         itm.addEventListener('dblclick', self.on_edit_item)
-        elm = doc.li('', childList=(
-            doc.div(className='view', childList=(
-                doc.checkbox(onchange=self.on_todo_check),
+        self.set_todo_label(itm, checked=checked)
+        elm = doc.li('', childList=[
+            doc.div(className='view', childList=[
+                doc.checkbox(onchange=self.on_todo_check, checked=checked),
                 itm,
                 doc.button('x', className='destroy', onclick=self.on_todo_destroy),
-            )),
-        ))
+            ]),
+        ])
+        selval = self.get_sel_value()
+        self.set_disp_style(elm, checked, selval)
         self.todo_list.appendChild(elm)
         self.set_left()
         self.new_todo.value = ''
+        sid = id(elm)
+        if store:
+            self.store_add(sid=sid, txt=txt, checked=False)
+        return sid
+
+    def get_store_id_by_label(self, label):
+        tgt = label.parent.parent
+        res = id(tgt)
+        return res
+
+    def get_store_id_by_done(self, check):
+        return self.get_store_id_by_label(check)
+
+    def get_store_id_by_destroy(self, check):
+        return self.get_store_id_by_label(check)
 
     def on_edit_item(self, ev):
         if 'targetId' not in ev:
@@ -137,7 +200,10 @@ class MyWindow(Window):
         if parent is None:
             return
         if done:
-            parent.textContent = target.value
+            txt = target.value
+            parent.textContent = txt
+            sid = self.get_store_id_by_label(parent)
+            self.store_mod(sid=sid, txt=txt)
         parent.classList.remove('editing')
         parent.removeChild(target)
 
@@ -162,23 +228,35 @@ class MyWindow(Window):
         li = self.get_li_by_chk(chk)
         self.todo_list.removeChild(li)
         self.set_left()
+        sid = self.get_store_id_by_destroy(chk)
+        self.store_del(sid)
 
     def on_todo_check(self, ev):
         chk = self.document.getElementById(ev['targetId'])
         checked = chk.checked
         self.change_check(chk, checked)
 
-    def change_check(self, chk, checked):
-        label = self.get_label_by_chk(chk)
+    def set_todo_label(self, label, checked):
+        key = 'text-decoration'
+        value = 'line-through'
         if checked:
-            label.style['text-decoration'] = 'line-through'
-        else:
-            label.style.pop('text-decoration')
+            label.style[key] = value
+        elif key in label.style:
+            label.style.pop(key)
+
+    def change_check(self, chk, checked):
+        selval = self.get_sel_value()
+        li = self.get_li_by_chk(chk)
+        self.set_disp_style(li, checked, selval)
+        label = self.get_label_by_chk(chk)
+        self.set_todo_label(label, checked)
         self.set_left()
+        sid = self.get_store_id_by_done(chk)
+        self.store_mod(sid=sid, checked=checked)
 
     def on_new_todo(self, ev):
         txt = self.new_todo.value
-        self.add_todo(txt)
+        self.add_todo(txt, checked=False, store=True)
 
     def on_toggle_all(self, ev):
         chk = self.document.getElementById(ev['targetId'])
@@ -195,18 +273,28 @@ class MyWindow(Window):
         self.count.textContent = cnt
         self.toggle_all.checked = 0 < all and 0 == cnt
 
+    def get_sel_value(self):
+        lst = self.document.getElementsByName('sel')
+        for itm in lst:
+            if itm.checked:
+                res = itm.value
+                return res
+        return None
+
+    def set_disp_style(self, li, done, selval):
+        display = (
+                (selval == 'All') or
+                ((selval == 'Completed') and done) or
+                ((selval == 'Active') and (not done)))
+        li.style['display'] = 'list-item' if display else 'None'
+
     def on_sel_change(self, ev):
         elm = self.document.getElementById(ev['targetId'])
         if elm is None:
             return
         for li in self.todo_list.childList:
             chk = self.get_chk_by_li(li)
-            done = chk.checked
-            display = (
-                (elm.value == 'All') or
-                ((elm.value == 'Completed') and done) or
-                ((elm.value == 'Active') and (not done)))
-            li.style['display'] = 'list-item' if display else 'None'
+            self.set_disp_style(li, chk.checked, elm.value)
 
     def on_clear_completed(self, ev):
         lst = [(self.get_chk_by_li(li), li) for li in self.todo_list.childList]
@@ -214,6 +302,8 @@ class MyWindow(Window):
             if chk.checked:
                 # self.todo_list.removeChild(li)
                 self.todo_list.childList.remove(li)
+                sid = id(li)
+                self.store_del(sid)
 
 
 def main():
@@ -222,7 +312,8 @@ def main():
 
 
 if __name__ == "__main__":
-    basicConfig(format='%(asctime)-15s %(module)s %(funcName)s %(levelname)s %(message)s')
+    basicConfig(format='%(asctime)-15s %(levelname)s %(module)s.%(funcName)s %(message)s')
     logger = getLogger()  # root logger
     logger.setLevel(DEBUG)
+    # logger.setLevel(INFO)
     main()
